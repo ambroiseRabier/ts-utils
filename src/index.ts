72c188262bc2c8faac0df8dbc8a8309210fbd578
Ambroise Rabier <rabier.ambroise@outlook.fr>
2019-07-20 17:33:31 +0200

    Fixed missing base re-export.

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,21 @@ import {AjaxStatus} from './lib/ajax-status';
 import {isMobile} from './lib/is-mobile';
 import {KeyEvents} from './lib/key-events';
 import {YoutubeUtils} from './lib/youtube.utils';
+import {DeepPartial} from './lib/deep-partial';
+import {HtmlInputType} from './lib/html-input.type';
+import {keysOf} from './lib/keys-of';
+import {pathof} from './lib/pathof';
+
+
 
 export {
   AjaxStatus,
+  DeepPartial,
+  HtmlInputType,
   isMobile,
   KeyEvents,
-  YoutubeUtils,
+  keysOf,
   nameof,
+  pathof,
+  YoutubeUtils,
 };
